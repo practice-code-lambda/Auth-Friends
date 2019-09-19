cf832200beb72232519fa63b8d3a93fc13837a91
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-09-18 23:33:57 -0500

    remove home link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,7 +8,6 @@ const Header = (props) => {
     return (
         <nav>
             <Link to={'/friends'}><li>Protected</li></Link>
-            <Link to={'/'}><li>Home</li></Link>
             <Link to={'/login'}><li>Login</li></Link>
             <Link to={'/addfriend'}><li>Add Friend</li></Link><link />
         </nav>
